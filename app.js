7b1e9bf34f8f1ce7b81efb17e4ab42bfcee2d8cd
kade <zaccaro.kade@gmail.com>
2024-01-07 19:29:12 -0500

    Add dropdown menu and image slider functionality

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// ******** DROP-DOWN MENU ********
+
 function toggleDropdown(dropdownId) {
   const dropdown = document.getElementById(dropdownId);
   dropdown.classList.toggle("show-menu");
@@ -10,3 +12,78 @@ function toggleDropdown(dropdownId) {
     downIcon.classList.remove("rotate-icon");
   }
 }
+
+// ******** IMAGE SLIDER ********
+
+const sliderDots = document.querySelectorAll(".dot");
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+let intervalId;
+
+// automatically move slider every 5 seconds
+
+function startSlider() {
+  intervalId = setInterval(() => {
+    if (currentSlide >= slides.length - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+    moveSlider(currentSlide);
+  }, 5000);
+}
+
+function moveSlider(index) {
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${index * 100}%)`;
+  });
+  activateSliderDots(index);
+}
+
+function activateSliderDots(index) {
+  sliderDots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+  sliderDots[index].classList.add("active");
+}
+
+function stopSlider() {
+  clearInterval(intervalId);
+}
+
+// manually move slider via dots
+
+sliderDots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    stopSlider();
+    moveSlider(index);
+  });
+});
+
+// anually move slider via arrows
+
+const arrowLeft = document.querySelector(".arrow-left");
+const arrowRight = document.querySelector(".arrow-right");
+
+arrowLeft.addEventListener("click", () => {
+  stopSlider();
+  if (currentSlide <= 0) {
+    currentSlide = slides.length - 1;
+  } else {
+    currentSlide--;
+  }
+  moveSlider(currentSlide);
+});
+
+arrowRight.addEventListener("click", () => {
+  stopSlider();
+  if (currentSlide >= slides.length - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+  moveSlider(currentSlide);
+});
+
+startSlider();
